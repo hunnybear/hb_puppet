aee18f21e9081a904b76e6da184e143fd7fdfe43
Tyler Jachetta <me@tylerjachetta.net>
2018-07-21 19:49:34 -0400

    some reshuffling and working on the bootstrap script

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+$BOOTSTRAP_ENV="bootstrap"
+
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install git puppetserver mod_passenger mod_ssl puppetdb puppet-dashboard mariadb-server
-puppet module install puppet-hiera
-puppet module install theforeman-puppet
-puppet module install theforeman-git
-git clone h6x6nmark@tylerjachetta.net:/home/h6x6nmark/git/hb_puppet 
+# TODO if check this
+mkdir "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}"
+git clone "h6x6nmark@tylerjachetta.net:/home/h6x6nmark/git/hb_puppet" "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}/modules/hb_puppet"
+puppet module install --environment $BOOTSTRAP_ENV puppet-hiera
+puppet module install --environment $BOOTSTRAP_ENV theforeman-puppet
+puppet module install --environment $BOOTSTRAP_ENV theforeman-git
