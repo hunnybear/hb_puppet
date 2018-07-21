998f1a6caea9caba8d169e26593c6ed352486856
Tyler Jachetta <me@tylerjachetta.net>
2018-07-21 19:59:46 -0400

    more puppet setup fixes

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -6,7 +6,7 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install git puppetserver mod_passenger mod_ssl puppetdb puppet-dashboard mariadb-server
 # TODO if check this
 mkdir "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}"
-git clone "h6x6nmark@tylerjachetta.net:/home/h6x6nmark/git/hb_puppet" "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}/modules/hb_puppet"
+git clone "h6x6nmark@tylerjachetta.net:/home/h6x6nmark/git/hb_puppet" "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}"
 puppet module install --environment $BOOTSTRAP_ENV puppet-hiera
 puppet module install --environment $BOOTSTRAP_ENV theforeman-puppet
 puppet module install --environment $BOOTSTRAP_ENV theforeman-git
