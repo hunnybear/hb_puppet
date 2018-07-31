620f100cfe940c8df8dfb96114b37d3a4f024625
Tyler Jachetta <me@tylerjachetta.net>
2018-07-30 20:45:07 -0400

    removing hb_puppet::enc manifest, as just installing the puppet_tools python module should get me there

--- modules/hb_puppet/manifests/enc.pp
+++
@@ -1,8 +0,0 @@
-class hb_puppet::enc($path=hiera("puppet::server_external_nodes", undef)) {
-	include python
-
-	# TODO do a git clone and then pip -e install for now
-	# TODO after getting that to work, figure out how to actually just get the 
-	#		git package artifacts
-
-}
