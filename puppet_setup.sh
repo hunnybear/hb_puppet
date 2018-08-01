976fa5d08f5cc11208513127033a2e2220977658
Tyler Jachetta <me@tylerjachetta.net>
2018-08-01 10:23:40 -0400

    doing some more work on the bootstrap script

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -3,6 +3,37 @@
 BOOTSTRAP_ENV="bootstrap"
 ENV_PATH="/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}"
 
+function setup_env() {
+	local start_wd=$(pwd)
+	local status_res
+
+	if [ -e "${ENV_PATH}" ]; then
+		cd "$ENV_PATH"
+		git status "$ENV_PATH"
+		status_res=$?
+
+		if [[ $status_res -ne 0 ]]; then
+			cd ..
+			rm -rf "${ENV_PATH}"
+		fi
+	fi
+
+	cd $start_wd
+
+	mkdir "${ENV_PATH}"
+
+	# TODO come up with some good way for modules with dependencies (that thus 
+	# shouldn't just be git submodule'd) to be regularly checked for updates
+
+	git clone --recursive "https://github.com/hunnybear/hb_puppet.git" "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}"
+	puppet module install --environment "$BOOTSTRAP_ENV" puppet-hiera
+	puppet module install --environment "$BOOTSTRAP_ENV" theforeman-puppet
+	puppet module install --environment "$BOOTSTRAP_ENV" theforeman-git
+	puppet module install --environment "$BOOTSTRAP_ENV" stahnma-epel --version 1.3.1
+
+}
+
+
 rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 
 # for mod_passenger
@@ -10,22 +41,9 @@ rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 # Removing some of the server-specific things, because those can be added by the bootstrap
 # yum install mod_passenger puppetdb puppet-dashboard mariadb-server mod_ssl
 
-yum install git puppetserver  
-# TODO if check this
-if [ -e "${ENV_PATH}" ]; then
-	rm -rf "${ENV_PATH}"
-fi
+yum install git puppetserver
 
-mkdir "${ENV_PATH}"
-
-# TODO come up with some good way for modules with dependencies (that thus 
-# shouldn't just be git submodule'd) to be regularly checked for updates
-
-git clone --recursive "https://github.com/hunnybear/hb_puppet.git" "/etc/puppetlabs/code/environments/${BOOTSTRAP_ENV}"
-puppet module install --environment "$BOOTSTRAP_ENV" puppet-hiera
-puppet module install --environment "$BOOTSTRAP_ENV" theforeman-puppet
-puppet module install --environment "$BOOTSTRAP_ENV" theforeman-git
-puppet module install --environment "$BOOTSTRAP_ENV" stahnma-epel --version 1.3.1
+setup_env
 
 # WIP, eventually remove noop
 FACTER_bootstrap=true
