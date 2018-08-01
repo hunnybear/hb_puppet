467480caf6e8d82feed7ebc7976970011fa04512
Tyler Jachetta <me@tylerjachetta.net>
2018-07-31 21:58:43 -0400

    suuuuper useful comment added

--- modules/hb_puppet/manifests/init.pp
+++ modules/hb_puppet/manifests/init.pp
@@ -13,6 +13,10 @@ class hb_puppet() {
 	}
 
 	# TODO datafy url
+	# TODO figure out whether I can work my way around doing the hb_python (the
+	# real needed require is Package[pip], but either way) require every time I 
+	# want to install a pip module. I think this is a global resource default,
+	# in site.pp.
 	python::pip{'puppet_tools':
 		url		=> 'git+https://github.com/hunnybear/puppet_tools.git',
 		require	=> Class['hb_python'],
